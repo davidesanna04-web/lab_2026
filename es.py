81e5745026e0c7c94fa9fecaec9d9e1d3f446da4
Davidesanna04 <davidesanna04@gmail.com>
2026-03-20 10:25:10 +0100

    lezione20_03

---
+++ es.py
@@ -0,0 +1,22 @@
+
+def compute_sum(
+        x: float,
+        y: float,
+        z: float = 0.0,
+        round: int | None=None,
+)-> float:
+    return x+y
+
+
+
+s:str= compute_sum(1,"ciao")
+
+print(s)
+
+
+a:dict[str, int]
+
+a: list | tuple | None | int
+
+
+
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+
 from fastapi import FastAPI
 
 app = FastAPI() #creo l'oggetto fastAPI
 
-@app.get("/hello") #metodo get per la richiesta, mettiamo / che signifca la radice ovvero l'home page. Se aggiungo hello e non metto solo /
+@app.get("/") #metodo get per la richiesta, mettiamo / che signifca la radice ovvero l'home page. Se aggiungo hello e non metto solo /
 #allora dovrò cercare il sito http://localhost:8000/hello e non http://localhost:8000/ 
-def hello_world():
-    return "Hello World!"  #il return è convertito direttamente in json
+def hello_world(q: str, 
+                sort:bool =False #mi dovrebbe dare errore perchè qui sort è bool ma nel dizionario è str dunque non va bene 
+                )->dict [str,str]:
+    return {"q":q, "sort":sort} #il return è convertito direttamente in json
+
+@app.get("/home")
+def homepage():
+    return "This is the home page"
 
 
 
+#anche qui obbiamo usare il typing
+@app.get("/{username}")
+def username_webpage(username:str):
+    return f"This in the webpage of user {username}"
 
+@app.get("/{username}/orders/{order_id}") #immaginiamo di essere in un negozio
 
-
-
-
-
+def repository_webpage(username:str,
+                       order_id:int,
+                       sort: bool = False):
+    return f"Order {order_id} for user {username}, Sorted:{sort}"
 
 
 

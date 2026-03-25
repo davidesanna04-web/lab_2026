8915d1873ebfce982168d532a17ffc416c5f6e50
Davidesanna04 <davidesanna04@gmail.com>
2026-03-25 09:08:58 +0100

    aggiornamento

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 
 
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+ 
+app=FastAPI()
+
+@app.get("/", response_class=HTMLResponse)#parameto che specifica in che formato fastapi deve convertire ilr isultato
+def home():
+
+
+
+    html="""
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1> Hello world! </h1>
+            <p> This is a simple FastAPI app.</p>
+        </body>
+    </html>
+    """
+    
+    return html 
+
+
+
+
+

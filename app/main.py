8933ee6db6dc6916a971e7ffeed9004d27da8ca7
Davidesanna04 <davidesanna04@gmail.com>
2026-05-06 08:45:42 +0200

    modifiche

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 
 from fastapi import FastAPI
 from routers.books import books_router
+from contextlib import asynccontextmanager
+from data.db import init_database
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #on create
+    init_database()
+    yield
 
+    #on close 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(books_router)
+
